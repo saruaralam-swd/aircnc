fe02f864c6ff515be98e0785011728ef77e24981
saruaralam-swd <saruaralam2001@gmail.com>
2023-07-21 09:13:36 +0600

    server init

--- server/index.js
+++ server/index.js
@@ -1,37 +1,41 @@
 const express = require("express");
 const cors = require("cors");
+const jwt = require("jsonwebtoken");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+require("colors");
 require("dotenv").config();
 
 const app = express();
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 5000;
 
-// middlewares
+// middleware
 app.use(cors());
 app.use(express.json());
 
-// Database Connection
+// Database Connected
 const uri = process.env.DB_URI;
 const client = new MongoClient(uri, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  serverApi: ServerApiVersion.v1,
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
 });
 
 async function run() {
   try {
-    const homesCollection = client.db("aircncdb").collection("homes");
-
-    console.log("Database Connected...");
+    const userCollection = client.db("AirCnc").collection("users");
+    console.log("Database Connected".yellow);
   } finally {
+    await client.close();
   }
 }
-
-run().catch((err) => console.error(err));
+run().catch((err) => console.log(err.name, err.message));
 
 app.get("/", (req, res) => {
-  res.send("Server is running...");
+  res.send("AirCnc server is running");
 });
 
 app.listen(port, () => {
-  console.log(`Server is running...on ${port}`);
+  console.log(`server is running on the port ${port}`.cyan);
 });
